39c06d7f13f284ba6ab0bbd55bbe054f925ac03a
Beline21 <159459656+Beline21@users.noreply.github.com>
2024-09-18 17:06:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,26 +2,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList <Integer>();
-        list.add(6);
-        list.add(5);
-        list.add(9);
-        list.add(7);
-        list.add(4);
-        list.add(2);
-        list.add(2);
-        //System.out.println(list);
-        //System.out.println(QuickSort.quickSort(list, 0, list.size()-1, "Beginning"));
-        //System.out.println(QuickSort.quickSort(list, "Median"));
 
-        // Tableaux dans le pire des cas
+        // Tableaux dans le pire des cas (déjà trié)
         for (int j = 1; j < 10; j++) {
             ArrayList<Integer> list2 = new ArrayList <Integer>();
-            for (int i = j; i >= 0; i--) {
+            for (int i = 0; i < j; i++) {
                 list2.add(i);
             }
-            System.out.println(list2);
-            System.out.println(QuickSort.quickSort(list2, "Beginning"));
+            //System.out.println(list2);
+            //System.out.println(QuickSort.quickSort(list2, "Beginning"));
+        }
+
+        // Tableaux remplis aléatoirement
+        for (int j = 1; j < 10; j++) {
+            ArrayList<Integer> list3 = new ArrayList <Integer>();
+            for (int i = 0; i < j; i++) {
+                list3.add((int)(Math.random() * 10));
+            }
+            System.out.println(list3);
+            //System.out.println(QuickSort.quickSort(list3, "Beginning"));
+            System.out.println(QuickSort.quickSort(list3, "Median"));
         }
     }
 }
